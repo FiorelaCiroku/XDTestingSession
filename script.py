d94a87a0e4ec1ddf9249bdceef528ccd43120530
Fiorela Ciroku <fiorelaciroku@yahoo.com>
2022-09-23 16:25:02 +0200

    Update script.py

--- script.py
+++ script.py
@@ -346,7 +346,7 @@ def createTestCaseAndDataSetFile(fileData, fileName, repoName):
         for indexTest in range(len(fileData['fragments'][indexFragment]['tests'])):
             if (getCheckValue(fileData, indexFragment, indexTest) is None or getCheckValue(fileData, indexFragment, indexTest) == 0):
                 testData = fileData['fragments'][indexFragment]['tests'][indexTest]
-                # print(testData)
+                print(testData)
                 if (getTestType(testData) == 'COMPETENCY_QUESTION'):
                     try:
                         # validateSPARQL(getQueryContent(testData))
@@ -384,7 +384,7 @@ def createTestCaseAndDataSetFile(fileData, fileName, repoName):
 
                         with open(testFilePath+'/CQDataSet/'+getID(testData)+'TD.ttl', 'w') as f:
                             f.write(
-                                getData(testData(test))
+                                getData(testData)
                             f.close()
                     except Exception as error:
                         setStatusValue(fileName, 'warning',
@@ -428,7 +428,7 @@ def createTestCaseAndDataSetFile(fileData, fileName, repoName):
                             f.close()
                         with open(testFilePath+'/CQDataSet/'+getID(testData)+'TD.ttl', 'w') as f:
                             f.write(
-                                getData(testData(test))
+                                getData(testData)
                             f.close()
                     except Exception as error:
                         setStatusValue(fileName, 'warning',
@@ -464,7 +464,7 @@ def createTestCaseAndDataSetFile(fileData, fileName, repoName):
                             f.close()
                         with open(testFilePath+'/CQDataSet/'+getID(testData)+'TD.ttl', 'w') as f:
                             f.write(
-                                getData(testData(test))
+                                getData(testData)
                             f.close()
                     except Exception as error:
                         setStatusValue(fileName, 'warning',
