dedfa7618b00768bdeda92994cab47a248f68333
Fiorela Ciroku <fiorelaciroku@yahoo.com>
2023-01-10 20:37:30 +0100

    Update script.py

--- script.py
+++ script.py
@@ -400,7 +400,7 @@ def createTestCaseAndDataSetFile(fileData, fileName, repoName):
                                 '\towlunit:testsOntology ns: .\n')
                             print(f.read())
                             f.close()
-
+                        print('URL per toy dataset', testFilePath+'/CQDataSet/'+getID(testData)+'TD.ttl)
                         with open(testFilePath+'/CQDataSet/'+getID(testData)+'TD.ttl', 'w') as f:
                             f.write(
                                 getData(testData))
